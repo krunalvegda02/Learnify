4b4dfcdc8d377996bc166e9aabeb4e7acdf34665
krunalvegda02 <Krunal.vegda02@gmail.com>
2025-01-06 22:20:57 +0530

    refactor: update environment configuration and database connection setup

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./src/db/db.js";
+
+dotenv.config({
+  path: "./.env",
+});
 
 const app = express();
 
+const PORT = process.env.PORT;
 
-const PORT = 8000;
-app.listen(PORT)
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB connnection failed: ", error);
+  });
