ed368e51a584c1c1587c9a60c549f36eef5729fb
krunalvegda02 <Krunal.vegda02@gmail.com>
2025-01-07 23:02:25 +0530

    feat: integrate Redux for user authentication with API endpoints

---
+++ Frontend/src/Redux/Features/Api/authApi.js
@@ -0,0 +1,40 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { useDispatch } from "react-redux";
+
+const dispatch = useDispatch();
+const USER_API = "http://localhost:3000/api/v1/user/";
+
+export const authApi = createApi({
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: USER_API,
+    credentials: "include",
+  }),
+  endpoints: (builder) => ({
+    //Mutation for posting data  and  Query for fetching data
+    registerUser: builder.mutation({
+      query: (inputData) => ({
+        url: "register",
+        method: "POST",
+        body: inputData,
+      }),
+    }),
+    LoginUser: builder.mutation({
+      query: (inputData) => ({
+        url: "login",
+        method: "POST",
+        body: inputData,
+      }),
+      async onQueryStarted(args, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+          dispatch(loginUser({ user: result.data.user }));
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    }),
+  }),
+});
+
+export const { useRegisterUserMutation, useLoginUserMutation } = authApi;
