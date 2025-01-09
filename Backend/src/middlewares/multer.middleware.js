826cd5159d31d10fcf185115ec1df0ba3c8467f4
krunalvegda02 <Krunal.vegda02@gmail.com>
2025-01-09 17:09:44 +0530

    CLoudinary and Multer added

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -1,12 +1,5 @@
 import multer from "multer";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/temp");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
+const upload = multer({ dest: "public/temp/" });
 
-export const upload = multer({ storage });
+export default upload;
